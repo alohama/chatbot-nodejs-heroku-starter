618e98995834a64189286a60519840784bcaa501
Zerojame <toypoyomn03@outlook.com>
2019-02-12 16:25:42 +0700

    push code

--- server.js
+++ server.js
@@ -1,126 +1,177 @@
 const express = require('express');
 const middleware = require('@line/bot-sdk').middleware
 const Client = require('@line/bot-sdk').Client
-
+const restClient = new (require('node-rest-client').Client)
 const app = express()
 
+
 const config = {
-    channelAccessToken: 'dj5Y4uKGAADe7C0mAy5ou2fUu7CgAJEcaga9woyL0qDZt68Gt+XEX+HULqFr2SO8MnOcIxvdAl7VLwmPBdnr17dXixLnQYPJbfwbTGQVmg1SWfpC9DZ2/gizSEWWinxFhBuocvuJWcMrN815sXmFjwdB04t89/1O/w1cDnyilFU=',
-    channelSecret: 'b834ee9fae4eece11de58efa8e6d14c1'
+    channelAccessToken: 'ClRAluwKDzDpg7MkEYhE/qcGO8U36hKgYs/KXHuAsGkeHg4Nd1wfHRD1cpda0EM6OOECwXstW6xYr0A7qaNj1x61TkB8EfV+v8TEnEmsx0DtRAfUYsvNCSb2Oij7wbAwAeFl8HnlXPYg0iUYVzvWhgdB04t89/1O/w1cDnyilFU=',
+    channelSecret: '75240895377b57eeed5bd387c2668508'
 }
+
 
 const client = new Client(config)
 
 app.get('/', function (req, res) {
-	res.send('chatbot-nodejs-heroku-starter!!');
+	res.send('chatbot-nodejs-heroku-starter!! kljlkjlkjhjkjhkj jknljlkhj');
 })
 
-app.post('/webhook', middleware(config), (req, res) => {
+app.use(middleware(config))
+
+app.post('/webhook', (req, res) => {
     res.sendStatus(200)
-    //  console.log(req.body.events) // webhook event objects
-    // console.log(req.body.destination) // user ID of the bot (optional))
-    return Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result) => console.log(result))
-    .catch((error) => console.log(error))
-})  
+    console.log(req.body.events) // webhook event objects
+    console.log(req.body.destination) // user ID of the bot (optional)
+    Promise
+        .all(req.body.events.map(hanndleEvent))
+})
 
-function handleEvent(event) {
-    console.log(event)
-    if(event.type === 'message') {
-        if(event.message.type === 'text') {
-            return Promise.resolve(handleMessageEvent(event))
-        }
-    }else {
-      return Promise.resolve(null)
+let name;
+let age;
+let question = 0;
+
+function hanndleEvent(event) {
+    if(question === 1) {
+        question = 2
+        name = event.message.text
+        return client.replyMessage(event.replyToken, {type: 'text', text: `คุณ ${name}อายุเท่าไหร่?`})
+    }else if(question === 2) {
+        question = 0
+        age = event.message.text
+        return client.replyMessage(event.replyToken, {type: 'text', text: `คุณ${name}อายุ ${age} เองครับ...`})
     }
-}
-
-function handleMessageEvent(event) {
-    // let msg = {
-    //     type: event.message.type,
-    //     text: event.message.text
-    // }
-    // client.pushMessage(event.source.userId, msg);
-    // return client.pushMessage(event.source.userId, msg);
-    let msg = { 
-        "type": "template",
-        "altText": "this is a carousel template",
-        "template": {
-            "type": "carousel",
-            "columns": [
-                {
-                  "thumbnailImageUrl": "https://vignette.wikia.nocookie.net/line/images/b/bb/2015-brown.png/revision/latest?cb=20150808131630",
-                  "imageBackgroundColor": "#FFFFFF",
-                  "title": "this is menu",
-                  "text": "description",
-                  "defaultAction": {
-                      "type": "uri",
-                      "label": "View detail",
-                      "uri": "http://example.com/page/123"
-                  },
-                  "actions": [
-                      {
-                          "type": "postback",
-                          "label": "Buy",
-                          "data": "action=buy&itemid=111"
-                      },
-                      {
-                          "type": "postback",
-                          "label": "Add to cart",
-                          "data": "action=add&itemid=111"
-                      },
-                      {
+    if(event.message.type === 'text' && event.message.text === 'location') {
+        let msg = {
+            "type": "location",
+            "title": "I GEAR GEEK",
+            "address": "malada place",
+            "latitude": 18.7777315,
+            "longitude": 98.9513933
+        }
+        //client.replyMessage(event.replyToken, msg)
+        client.pushMessage(event.source.userId, msg)
+        return client.pushMessage(event.source.userId, msg)
+    }else if(event.message.type === 'text' && event.message.text === 'menu') {
+        let msg = {
+            "type": "template",
+            "altText": "this is a carousel template",
+            "template": {
+                "type": "carousel",
+                "columns": [
+                    {
+                      "thumbnailImageUrl": "https://vignette.wikia.nocookie.net/line/images/b/bb/2015-brown.png/revision/latest?cb=20150808131630",
+                      "imageBackgroundColor": "#4286f4",
+                      "title": "this is menu",
+                      "text": "description",
+                      "defaultAction": {
                           "type": "uri",
                           "label": "View detail",
-                          "uri": "http://example.com/page/111"
-                      }
-                  ]
-                },
-                {
-                  "thumbnailImageUrl": "https://vignette.wikia.nocookie.net/line/images/b/bb/2015-brown.png/revision/latest?cb=20150808131630",
-                  "imageBackgroundColor": "#000000",
-                  "title": "this is menu",
-                  "text": "description",
-                  "defaultAction": {
-                      "type": "uri",
-                      "label": "View detail",
-                      "uri": "http://example.com/page/222"
-                  },
-                  "actions": [
-                      {
-                          "type": "postback",
-                          "label": "Buy",
-                          "data": "action=buy&itemid=222"
+                          "uri": "http://example.com/page/123"
                       },
-                      {
-                          "type": "postback",
-                          "label": "Add to cart",
-                          "data": "action=add&itemid=222"
-                      },
-                      {
+                      "actions": [
+                          {
+                              "type": "postback",
+                              "label": "Buy",
+                              "data": "action=buy&itemid=111"
+                          },
+                          {
+                              "type": "postback",
+                              "label": "Add to cart",
+                              "data": "action=add&itemid=111"
+                          },
+                          {
+                              "type": "uri",
+                              "label": "View detail",
+                              "uri": "http://example.com/page/111"
+                          }
+                      ]
+                    },
+                    {
+                      "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+                      "imageBackgroundColor": "#000000",
+                      "title": "this is menu",
+                      "text": "description",
+                      "defaultAction": {
                           "type": "uri",
                           "label": "View detail",
                           "uri": "http://example.com/page/222"
-                      }
-                  ]
-                }
-            ],
-            "imageAspectRatio": "rectangle",
-            "imageSize": "cover"
-        }
+                      },
+                      "actions": [
+                          {
+                              "type": "postback",
+                              "label": "Buy",
+                              "data": "action=buy&itemid=222"
+                          },
+                          {
+                              "type": "postback",
+                              "label": "Add to cart",
+                              "data": "action=add&itemid=222"
+                          },
+                          {
+                              "type": "uri",
+                              "label": "View detail",
+                              "uri": "http://example.com/page/222"
+                          }
+                      ]
+                    }
+                ],
+                "imageAspectRatio": "rectangle",
+                "imageSize": "cover"
+            }
+          }
+        return client.replyMessage(event.replyToken, msg)
+    }else if(event.message.type === 'text' && event.message.text === 'สวัสดี') {
+        question = 1
+        return client.replyMessage(event.replyToken, { type : "text", text: "สวัสดีคุณชื่ออะไร?"})
+    }else if(event.message.type === 'text' && event.message.text === 'บาย') {
+        question = 0
+        return client.replyMessage(event.replyToken, { type : "text", text: "จบการสนทนา"})
     }
-
-    let msg2 = { 
-            "type":"text",
-            "text": "dasd"
+    if(event.type === 'message' && event.message.type === 'location') {
+        return handleLocationEvent(event)
     }
-
-    return client.replyMessage(event.replyToken, msg2);
-        // for(var i= 0;i< 10;i++) {
-        //     client.pushMesjnsage(event.source.userId, { type: "text", text: 'hello'})
-        // }
 }
+
+
+function handleLocationEvent(event) {
+    apiUrl = 'https://fathomless-reaches-36581.herokuapp.com/api'
+    return new Promise((resolve, reject) => {
+    restClient.get(`${apiUrl}?lat=${event.message.latitude}&long=${event.message.longitude}`, (data, response) => {
+        if (data) {
+          const pinData = data.map(row => ({
+            "thumbnailImageUrl": row.aqi.icon,
+            "imageBackgroundColor": "#FFFFFF",
+            "title": `PM 2.5: ${row.aqi.aqi}`,
+            "text": `${row.nameTH}, ${row.areaTH}`,
+            "actions": [
+              {
+                "type": "uri",
+                "label": "ข้อมูลย้อนหลัง",
+                "uri": row.historyUrl
+              }
+            ]
+          }))
+      
+          var msg = {
+            "type": "template",
+            "altText": "ข้อมูลสถานที่",
+            "template": {
+              "type": "carousel",
+              "columns": pinData,
+              "imageAspectRatio": "rectangle",
+              "imageSize": "cover"
+            }
+          }
+  
+          resolve(client.replyMessage(event.replyToken, msg))
+        } else {
+          reject()
+        }
+      })
+    })
+   
+  }
 
 app.set('port', (process.env.PORT || 4000))
 
